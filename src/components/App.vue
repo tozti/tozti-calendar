e733cdd435298a19b9400804bd8631a86792017e
poechsel <pierre.oechsel@gmail.com>
2018-02-05 19:38:16 +0100

    Added placeholders for the calendar's component.
    - the dropdown menu allow to select the calendar you want to display
    - please modify either "src/components/calendarday.vue" or "src/components/calendarmonth.vue" or "src/components/calendarweek.vue"

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,31 +1,100 @@
 <template>
     <section>
-        <button class="button is-primary is-medium"
-            @click="isComponentModalActive = true">
-            Ajouter un evenement
-        </button>
+        <nav class="level">
+            <div class="level-left">
+                <p class="level-item"><a class="button">prev</a></p>
+                <p class="level-item"><a class="button">next</a></p>
+            </div>
+
+
+            <div class="level-right">
+                <p class="level-item">
+                <button class="button is-primary"
+                        @click="isComponentModalActive = true">
+                    Ajouter un evenement
+                </button>
+                </p>
+                <p class="level-item">
+                <button class="button is-primary">
+                    Plus d'informations
+                </button>
+                </p>
+                <p class="level-item">
+                <b-dropdown v-model="scaleCalendar" hoverable>
+                    <button class="button is-info" slot="trigger">
+                        <template v-if="scaleCalendar === 1">
+                            <span>Week</span>
+                        </template>
+                        <template v-else-if="scaleCalendar === 0">
+                            <span>Day</span>
+                        </template>
+                        <template v-else>
+                            <span>Month</span>
+                        </template>
+                        <b-icon icon="menu-down"></b-icon>
+                    </button>
+
+                    <b-dropdown-item :value=0>
+                        <div class="media">
+                            Day
+                        </div>
+                    </b-dropdown-item>
+                    <b-dropdown-item :value=1>
+                        <div class="media">
+                            Week
+                        </div>
+                    </b-dropdown-item>
+                    <b-dropdown-item :value=2>
+                        <div class="media">
+                            Month
+                        </div>
+                    </b-dropdown-item>
+                </b-dropdown>
+                </p>
+            </div>
+        </nav>
 
         <b-modal :active.sync="isComponentModalActive" has-modal-card>
             <modal-form v-bind="formProps"></modal-form>
         </b-modal>
+
+        <template v-if="scaleCalendar===0">
+            <calendar-day>
+            </calendar-day>
+        </template>
+        <template v-else-if="scaleCalendar===1">
+            <calendar-week>
+            </calendar-week>
+        </template>
+        <template v-else>
+            <calendar-month>
+            </calendar-month>
+        </template>
     </section>
 </template>
 
 <script>
-    import ModalForm from './EventCreationModal.vue'
+import ModalForm from './EventCreationModal.vue'
+import CalendarWeek from './CalendarWeek.vue'
+import CalendarDay from './CalendarDay.vue'
+import CalendarMonth from './CalendarMonth.vue'
 
-    export default {
-        components: {
-            ModalForm
-        },
-        data() {
-            return {
-                isComponentModalActive: false,
-                formProps: {
-                    email: 'evan@you.com',
-                    password: 'testing'
-                }
-            }
+export default {
+    components: {
+        ModalForm,
+        CalendarWeek,
+        CalendarMonth,
+        CalendarDay,
+    },
+    data() {
+        return {
+            isComponentModalActive: false,
+            formProps: {
+                email: 'evan@you.com',
+                password: 'testing'
+            },
+            scaleCalendar: 1,
         }
     }
+}
 </script>
---
+++ src/components/CalendarDay.vue
@@ -0,0 +1,27 @@
+<template>
+    <!-- REPLACER LE CONTENU -->
+    <!-- merci de prefixer les class & id par tcd 
+        (pour ToztiCalendarDay). Histoire d'éviter des 
+        conflits -->
+    <div class="tcd-placeholder">
+        ceci est un calendrier jour par jour
+    </div>
+    <!-- FIN DU CONTENU -->
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+            }
+        }
+    }
+</script>
+
+<style>
+.tcd-placeholder {
+    width: 500px;
+    height: 500px;
+    background-color: blue;
+}
+</style>
