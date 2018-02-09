3400d45e177cc424afcfe42cf6919f63214964d7
Vincent Rébiscoul <vincent.rebiscoul@ens-lyon.fr>
2018-02-09 19:05:51 +0100

    Add datepicker and timepicker

--- src/components/ViewEvent.vue
+++ src/components/ViewEvent.vue
@@ -2,10 +2,27 @@
   <div>
    <b-field label="Title">
             <b-input maxlength="50"  v-model="title" v-bind:disabled="!modifiable"></b-input>
-        </b-field>
-     <b-field label="Groups">   
-	
-	
+   </b-field>
+   
+    <b-field label="Select a date">
+        <b-datepicker
+            placeholder="Type or select a date..."
+            icon="calendar-today"
+            v-bind:disabled="!modifiable"
+	    v-model="time">
+        </b-datepicker>
+    </b-field>
+
+    <b-field label="Select timepicker">
+        <b-timepicker
+            placeholder="Type or select a date..."
+            icon="clock"
+	    v-bind:disabled="!modifiable"
+	    v-model="time">
+        </b-timepicker>
+    </b-field>
+   
+   <b-field label="Groups">   
 	<b-taginput
                 v-model="taggedgroups":data="groups"
                 v-bind:disabled="!modifiable"
@@ -33,14 +50,15 @@
         
         data(){
         	return {
-        	title:"Pinata Party",
-        		modifiable:false,
-        		description:"Pinata Party avec toutes les associations de l'ENS!",
+        	        title:"Pinata Party",
+        	        modifiable:false,
+        	        description:"Pinata Party avec toutes les associations de l'ENS!",
         		taggedgroups: 
         			[{"name":"AliENS"},{"name":"BDE"},{"name":"AS"}],
         		groups:
         			[{"name":"AliENS"},{"name":"BDE"},{"name":"AS"},{"name":"ArcENSiel"},{"name":"Unknown"}],
-        		allowNew:false
+        	        allowNew:false,
+		        time: new Date()
         	}
         }
     }
