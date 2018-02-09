31d0a26d6620d11a1a64c40afa3d07af7d45a7e3
Vincent Rébiscoul <vincent.rebiscoul@ens-lyon.fr>
2018-02-09 13:32:23 +0100

    add ViewEvent vue

---
+++ src/components/ViewEvent.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    
+    <div id="toplayer">
+      <h1>Titre</h1>
+    </div>
+    
+    <div id="content">
+      <p>Hello</p>
+      <div id="app">
+	<ul>
+	  <li v-for="group in groups">
+	    {{groups.text}}
+	  </li>
+	  
+	</ul>
+      </div>
+      
+      <textarea v-bind:disabled="!modifiable">Swag swag swag
+      </textarea>
+    </div>
+
+</div>
+</template>
+
+<script>
+  new Vue({
+  el:"#app",
+  data:{
+  
+  }
+  })
+    export default {
+        data() {groups: [{text:"AliENS"},{text:"BDE"},{text:"AS"}]
+            return {
+            }
+        }
+    }
+</script>
