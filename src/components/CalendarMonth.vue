566f0045f7767d04135dd1972139704dae150661
Guillaume Duboc <guilduboc@gmail.com>
2018-02-14 16:58:58 +0100

    Basic nonresponsive template for calendar-month

--- src/components/CalendarMonth.vue
+++ src/components/CalendarMonth.vue
@@ -4,7 +4,98 @@
         (pour ToztiCalendarMonth). Histoire d'éviter des 
         conflits -->
     <div class="tcm-placeholder">
-        ceci est un calendrier mois par mois
+         <div class="tcm-placeholder">
+          <section id = "tcm-calendar-month">
+            <div id = "tcm-calendar-month-header">
+                <div id = "spacer"></div>
+                <div class = "tcm-calendar-title-day">Lundi</div>
+                <div class = "tcm-calendar-title-day">Mardi</div>
+                <div class = "tcm-calendar-title-day">Mercredi</div>
+                <div class = "tcm-calendar-title-day">Jeudi</div>
+                <div class = "tcm-calendar-title-day">Vendredi</div>
+                <div class = "tcm-calendar-title-day">Samedi</div>
+                <div class = "tcm-calendar-title-day">Dimanche</div> 
+            </div>
+            <div id = "tcm-calendar-month-content">
+                    <div class = "tcm-calendar-entry">
+                    </div>
+                    <div class = "tcm-calendar-entry">1
+                    </div>
+                    <div class = "tcm-calendar-entry">2
+                    </div>
+                    <div class = "tcm-calendar-entry">3
+                    </div>
+                    <div class = "tcm-calendar-entry">4
+                    </div>
+                    <div class = "tcm-calendar-entry">5
+                    </div>
+                    <div class = "tcm-calendar-entry">6
+                    </div>
+                    <div class = "tcm-calendar-entry">7
+                    </div>
+                
+                
+                    <div class = "tcm-calendar-entry">
+                    </div>
+                    <div class = "tcm-calendar-entry">9
+                    </div>
+                    <div class = "tcm-calendar-entry">10
+                    </div>
+                    <div class = "tcm-calendar-entry">11
+                    </div>
+                    <div class = "tcm-calendar-entry">12
+                    </div>
+                    <div class = "tcm-calendar-entry">13
+                    </div>
+                    <div class = "tcm-calendar-entry">14
+                    </div>
+                    <div class = "tcm-calendar-entry">15
+                    </div>
+                
+                
+                    <div class = "tcm-calendar-entry">
+                    </div>
+                    <div class = "tcm-calendar-entry">16
+                    </div>
+                    <div class = "tcm-calendar-entry">17
+                    </div>
+                    <div class = "tcm-calendar-entry">18
+                    </div>
+                    <div class = "tcm-calendar-entry">19
+                    </div>
+                    <div class = "tcm-calendar-entry">20
+                    </div>
+                    <div class = "tcm-calendar-entry">21
+                    </div>
+                    <div class = "tcm-calendar-entry">22
+                    </div>
+                    <div class = "tcm-calendar-entry">
+                    </div>
+                    <div class = "tcm-calendar-entry">23
+                    </div>
+                    <div class = "tcm-calendar-entry">24
+                    </div>
+                    <div class = "tcm-calendar-entry">25
+                    </div>
+                    <div class = "tcm-calendar-entry">26
+                    </div>
+                    <div class = "tcm-calendar-entry">27
+                    </div>
+                    <div class = "tcm-calendar-entry">28
+                        </div>
+                    <div class = "tcm-calendar-entry">29
+                        </div>
+                
+                
+                    <div class = "tcm-calendar-entry">
+                        </div>
+                    <div class = "tcm-calendar-entry">30
+                            </div>
+                    <div class = "tcm-calendar-entry">31
+                            </div>
+            </div>
+        </section>
+    </div>
     </div>
     <!-- FIN DU CONTENU -->
 </template>
