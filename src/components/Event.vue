33a7baa573bbb971670769f2dfd5606bf13b3e2e
poechsel <pierre.oechsel@gmail.com>
2018-02-16 23:04:11 +0100

    drag by subdivision of 15min. Resize broken

--- src/components/Event.vue
+++ src/components/Event.vue
@@ -12,7 +12,9 @@ export default {
     props: ['gridDom', 'row', 'col'],
     created: function () {
         this.dragged = false
-        this.cell = {'row': this.row, 'col': this.col}
+        this.day = 0
+        this.start = {'hour': 0, 'quarter': 0}
+        this.end = {'hour': 0, 'quarter': 0}
         this.resized = false
     },
     mounted: function () {
@@ -21,11 +23,7 @@ export default {
         document.documentElement.addEventListener('mousedown', this.deselect, true)
         document.documentElement.addEventListener('mouseup', this.handleUp, true)
 
-        let cell = this.gridDom[this.cell.row][this.cell.col]
-        this.$el.style.top = cell.offsetTop + "px"
-        this.$el.style.left = cell.offsetLeft + "px"
-        this.$el.style.width = cell.style.width
-        this.$el.style.height = cell.style.height
+        this.updateDisplay()
     },
     beforeDestroy: function () {
         document.documentElement.removeEventListener('mousemove', this.handleMove, true)
@@ -39,6 +37,15 @@ export default {
     },
 
     methods: {
+
+        updateDisplay() {
+            let start_cell = this.gridDom[this.start.hour][this.day]
+            this.$el.style.left = start_cell.offsetLeft + "px"
+            this.$el.style.width = start_cell.offsetWidth + "px"
+            this.$el.style.top = (start_cell.offsetTop + (start_cell.offsetHeight * (this.start.quarter / 4))) + "px"
+            console.log(this.$el.style.top)
+        },
+
         handleUp (e) {
             let pos = this.getMousePos(e)
             this.pauseEvent(e)
@@ -55,10 +62,12 @@ export default {
             this.pauseEvent(e)
             console.log("foo")
             this.dragged = true;
-            let owner_cell = this.cell
+            let owner_cell = {'row': this.start.hour, 'col': this.day, 'sub': this.start.quarter}
             let mouse_cell = this.getHoveredCell(pos)
+            console.log(mouse_cell.sub, owner_cell.sub)
             this.offset = {'col': mouse_cell.col - owner_cell.col,
-                           'row': mouse_cell.row - owner_cell.row}
+                           'row': mouse_cell.row - owner_cell.row,
+                            'sub': mouse_cell.sub - owner_cell.sub}
         },
 
         handleMove: function (e) {
@@ -68,17 +77,19 @@ export default {
                 let cell_infos = this.getHoveredCell(pos)
                 if (cell_infos != null) {
                     let cell_coords = {'col': cell_infos.col - this.offset.col,
-                                       'row': cell_infos.row - this.offset.row}
+                                       'row': cell_infos.row - this.offset.row,
+                        'sub': cell_infos.sub - this.offset.sub}
                     if (this.checkDragValidity(cell_coords)) {
                         let cell = this.gridDom[cell_coords.row][cell_coords.col]
                         /*
                         this.$el.parentNode.removeChild(this.$el)
                         cell.appendChild(this.$el)
                         */
-                        this.cell = cell_coords
-                        this.$el.style.top = cell.offsetTop + "px"
-                        this.$el.style.left = cell.offsetLeft + "px"
-                        this.$el.style.width = cell.offsetWidth + "px"
+                        this.day = cell_coords.col
+                        this.start.hour = cell_coords.row
+                        this.start.quarter = cell_coords.sub 
+                        console.log(cell_coords.sub)
+                        this.updateDisplay()
                     }
                 }
             } 
@@ -86,10 +97,9 @@ export default {
                 let cell_infos = this.getHoveredCell(pos)
                 if (cell_infos != null) {
                     // + 1 because we can have a "single row cell"
-                    let size = cell_infos.el.offsetTop + cell_infos.el.offsetHeight - this.gridDom[this.cell.row][this.cell.col].offsetTop;
                     //let size = cell_infos.row - this.cell.row + 1
-                    if (size > 0) {
-                        this.$el.style.height = size + "px"
+                    if (cell_infos.row >= this.start.hour) {
+                        this.end.hour = cell_infos.row
                         //this.$el.style.height = size * 100 + "%"
                     }
                 }
@@ -129,12 +139,18 @@ export default {
                 for (let col_i = 0; col_i < this.gridDom[row_i].length; ++col_i) {
                     let boundrect = this.gridDom[row_i][col_i].getBoundingClientRect()
                     if (this.contain(this.gridDom[row_i][col_i], position)) {
-                        return {"el": this.gridDom[row_i][col_i], 'row': row_i, 'col': col_i}
+                        return {"el": this.gridDom[row_i][col_i], 'row': row_i, 'col': col_i, 'sub': this.getSubRow(this.gridDom[row_i][col_i], position)}
 
                     }
                 }
             }
             return null
+        },
+
+        getSubRow(cell, position) {
+            const boundrect = cell.getBoundingClientRect()
+            const fraction = (position.y - boundrect.y) / (boundrect.height);
+            return (4 * fraction) | 0
         },
 
         getMousePos(e) {
@@ -162,6 +178,8 @@ export default {
     bottom: 0; 
     width: 100%; 
     height: 25px; 
+    margin: 0;
+    padding: 0;
     background-color:red;
 }
 </style>
