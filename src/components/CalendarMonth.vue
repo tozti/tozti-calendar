4d36eb927aa3e0140fbb54b77ecfef085194d7e0
Guillaume Duboc <guilduboc@gmail.com>
2018-02-19 00:06:53 +0100

    Binding month rendering to current month

--- src/components/CalendarMonth.vue
+++ src/components/CalendarMonth.vue
@@ -4,25 +4,17 @@
         (pour ToztiCalendarMonth). Histoire d'éviter des 
         conflits -->
 
-
-
-
-
     <div class="tcm-placeholder">
          <div class="tcm-placeholder">
           <section id = "tcm-calendar-month">
+            
             <div id = "tcm-calendar-month-header">
                 <div id = "spacer"></div>
                 <div v-for="titleDay in weekDays" class = "tcm-calendar-title-day">{{titleDay}}</div>
             </div>
-            <div id = "tcm-calendar-month-content">
-                    <div v-for="day in firstWeek" class = "tcm-calendar-entry">{{day}}</div>
-                
-                    <div v-for="day in secondWeek" class = "tcm-calendar-entry">{{day}}</div>
-                
-                    <div v-for="day in thirdWeek" class = "tcm-calendar-entry">{{day}}</div>
 
-                    <div v-for="day in fourthWeek" class = "tcm-calendar-entry">{{day}}</div>
+            <div id = "tcm-calendar-month-content">
+                    <div v-for="day in firstWeeks" class = "tcm-calendar-entry">{{day}}</div>
     
                     <div v-for="day in fifthWeek" class = "tcm-calendar-entry">{{day}}</div>
             </div>
@@ -33,22 +25,25 @@
 </template>
 
 <script>
+import { computeFifthWeek } from './utils.js'
 
+var data = {monthDate : new Date("October 13, 2014 11:13:00")}
+
+data.monthDate = new Date("February 13, 2014 11:13:00")
 export default {
-    data() {
+    data() {        
         return { 
-            // first element is void to create first column
-            firstWeek : [,1,2,3,4,5,6,7],
-            secondWeek : [,8,9,10,11,12,13,14],
-            thirdWeek : [,15,16,17,18,19,20,21],
-            fourthWeek : [,22,23,24,25,26,27,28],
-            fifthWeek : [,29,30,31],
+            // void elements to create first column
+            firstWeeks : [,1,2,3,4,5,6,7,,8,9,10,11,12,13,14,,15,16,17,18,19,20,21,,22,23,24,25,26,27,28],
+            fifthWeek : computeFifthWeek(data.monthDate),
 
             weekDays : ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"]
         }
     },
-    components: {
-        CalendarMonthCell
+    methods: {
+        assignMonth: function (newDate) {
+            this.monthDate = newDate
+        }
     }
 }    
 </script>
---
+++ src/components/utils.js
@@ -0,0 +1,25 @@
+function leapYear(year)
+{
+  return ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
+}
+
+function daysInMonth(anyDateInMonth) {
+    return new Date(anyDateInMonth.getYear(), 
+                    anyDateInMonth.getMonth()+1, 
+                    0).getDate();}
+
+export function computeFifthWeek(date) {
+    let daysleft = (daysInMonth(date)-28)
+    let out = [,]
+    if (daysleft >= 0) {}
+    if (daysleft >= 1) {
+        out.push(29)
+    }
+    if (daysleft >= 2) {
+        out.push(30)
+    }
+    if (daysleft >= 3){
+        out.push(31)
+    }
+    return out
+}
