f66dcf242b70818ae2d8efdddbe8f5a3a2882a9b
Vincent Rébiscoul <vincent.rebiscoul@ens-lyon.fr>
2018-02-25 19:07:25 +0100

    fixed indentations

--- src/components/ModifyEvent.vue
+++ src/components/ModifyEvent.vue
@@ -1,64 +1,64 @@
 <template>
 	<div>
-	  <b-field label="Nom de l'événement">
-        <b-input maxlength="50"  v-model="title"></b-input>
-	  </b-field>
-	  
-      <b-field grouped>
-        <b-field label="Début" expanded>
-          <b-field>
-            <b-datepicker
-              placeholder="Sélectionner"
-              icon="calendar-today"
-			  v-model="time">
-            </b-datepicker>
-            <b-timepicker
-			  icon="clock"			  
-			  v-model="time"
-              required>
-            </b-timepicker>
-          </b-field>
-        </b-field>
-        <b-field label="Fin" expanded>
-          <b-field>
-            <b-datepicker
-              placeholder="Sélectionner"
-              icon="calendar-today">
-            </b-datepicker>
-            <b-timepicker
-			  icon="clock"			  
-			  v-model="time"
-              required>
-            </b-timepicker>
-          </b-field>
-        </b-field>
-      </b-field>
-
-	  <div class="field">
-        <b-switch v-model="recurrent">Événement récurrent</b-switch>
-      </div>
-	  
-	  <b-field label="Groupes">   
-		<b-taginput
-          v-model="taggedgroups":data="groups"
-          field="name"
-          autocomplete
-          :allowNew="allowNew"
-          icon="label"
-          placeholder="Add a group"
-          @typing="getFilteredGroups">
-        </b-taginput>
-	  </b-field>
-      <b-field label="Description">
-        <b-input maxlength="200" type="textarea" v-model="description"></b-input>
-      </b-field>
-
-	  <b-field label="Couleur">
-		<div>
-		  <swatches v-model="color" />
+		<b-field label="Nom de l'événement">
+			<b-input maxlength="50"  v-model="title"></b-input>
+		</b-field>
+		
+		<b-field grouped>
+			<b-field label="Début" expanded>
+				<b-field>
+					<b-datepicker
+						placeholder="Sélectionner"
+						icon="calendar-today"
+						v-model="time">
+					</b-datepicker>
+					<b-timepicker
+						icon="clock"			  
+						v-model="time"
+						required>
+					</b-timepicker>
+				</b-field>
+			</b-field>
+			<b-field label="Fin" expanded>
+				<b-field>
+					<b-datepicker
+						placeholder="Sélectionner"
+						icon="calendar-today">
+					</b-datepicker>
+					<b-timepicker
+						icon="clock"			  
+						v-model="time"
+						required>
+					</b-timepicker>
+				</b-field>
+			</b-field>
+		</b-field>
+		
+		<div class="field">
+			<b-switch v-model="recurrent">Événement récurrent</b-switch>
 		</div>
-	  </b-field>
-      
+		
+		<b-field label="Groupes">   
+			<b-taginput
+				v-model="taggedgroups":data="groups"
+				field="name"
+				autocomplete
+				:allowNew="allowNew"
+				icon="label"
+				placeholder="Add a group"
+				@typing="getFilteredGroups">
+			</b-taginput>
+		</b-field>
+		<b-field label="Description">
+			<b-input maxlength="200" type="textarea" v-model="description"></b-input>
+		</b-field>
+		
+		<b-field label="Couleur">
+			<div>
+				<swatches v-model="color" />
+			</div>
+		</b-field>
+		
 	</div>
 </template>
 
--- src/components/ViewEvent.vue
+++ src/components/ViewEvent.vue
@@ -1,36 +1,36 @@
 <template>
     <div class="content">
-	<h3 class="title is-3"><span :style="hexa_event_color">Événement: {{ title }}</span></h3>
-
-	  <div class="columns">
-		<div class="column">
-		  <h4 class="title is-4">Début</h4>
-		  <p>{{ beginning.toLocaleDateString() }}</p>
+		<h3 class="title is-3"><span :style="hexa_event_color">Événement: {{ title }}</span></h3>
+		
+		<div class="columns">
+			<div class="column">
+				<h4 class="title is-4">Début</h4>
+				<p>{{ beginning.toLocaleDateString() }}</p>
+			</div>
+			
+			<div class="column">		
+				<h4 class="title is-4">Fin</h4>
+				<p>{{ end.toLocaleDateString() }}</p>
+			</div>
 		</div>
-
-		<div class="column">		
-		  <h4 class="title is-4">Fin</h4>
-		  <p>{{ end.toLocaleDateString() }}</p>
-		</div>
-	  </div>
-	  	  
-	  <p v-if="recurrent">
-		Cet événement est récurrent.
-	  </p>
-	  
-	  <p v-else>
-		Cet événement n'est pas récurrent.
-	  </p>
-	  
-	  <h4 class="title is-4">Groupes</h4>
-	  <b-taglist>
-		<b-tag type="is-info" v-for="group in taggedgroups" :key="group.name">{{ group.name }}</b-tag>
-	  </b-taglist>
-	  
-	  <h4 class="title is-4">Description</h4>
-	  <p>{{ description }}</p>
-
-     
+	  	
+		<p v-if="recurrent">
+			Cet événement est récurrent.
+		</p>
+		
+		<p v-else>
+			Cet événement n'est pas récurrent.
+		</p>
+		
+		<h4 class="title is-4">Groupes</h4>
+		<b-taglist>
+			<b-tag type="is-info" v-for="group in taggedgroups" :key="group.name">{{ group.name }}</b-tag>
+		</b-taglist>
+		
+		<h4 class="title is-4">Description</h4>
+		<p>{{ description }}</p>
+		
+		
     </div>
 </template>
 
