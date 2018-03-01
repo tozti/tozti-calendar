80c583179f20805a42df253674a1c2561bfce927
Romain Liautaud <romain.liautaud@ens-lyon.fr>
2018-03-02 00:40:44 +0100

    Added a workspace menu item.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import App from './components/App.vue';
 
-
+// TODO(liautaud): This should be done using the proper API.
 tozti.addRoutes([
- { path: '/calendar', component: App }
+ { path: '/calendar', component: App },
+ { path: '/workspaceCalendar', component: App }
 ])
-tozti.addMenuItem("Calendar", "/calendar", { icon: 'nc-calendar-60' })
+tozti.addMenuItem("Calendrier", "/calendar", { icon: 'nc-calendar-60' })
+tozti.addWorkspaceMenuItem("Calendrier", "/workspaceCalendar", { icon: 'nc-calendar-60' })
